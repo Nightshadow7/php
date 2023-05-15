f2570d36807e9fbaf149747a5f2cefd321ceb08d
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-15 13:17:45 -0500

    feat: :rocket: mi primer commit en php
    el mejor commit del munmdo mundial en php

---
+++ intro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ejemplo</title>
+</head>
+<body>
+  
+  <?php 
+    $a = "Mundo";
+    print($a);
+    echo " <h1>titulo generado desde php <br></h1>";
+    echo phpversion();
+    /* echo phpinfo(); */
+    $variable = " <br>hello world Duuuuuuuuuuuuuuuuuuude";
+    echo $variable;
+
+    //warning
+    echo $variableim;
+  ?>
+</body>
+</html>
